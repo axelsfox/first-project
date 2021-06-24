92d6a6a51896f971e47c4bcd00df2555bd7888a3
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-06-24 19:12:11 +0300

    +

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <p>Простой калькулятор</p>
-    <input class="inputField" type="text" placeholder="число 1" />
+    <input
+      class="inputField"
+      v-model.number.trim="operand1"
+      type="text"
+      placeholder="число 1"
+    />
     <div class="mathBtnBox">
-      <button class="mathBtn"></button>
-      <button class="mathBtn"></button>
-      <button class="mathBtn"></button>
-      <button class="mathBtn"></button>
-      <button class="mathBtn"></button>
+      <button class="mathBtn" @click="summary">+</button>
+      <button class="mathBtn" @click="subtraction">-</button>
+      <button class="mathBtn" @click="multiply">*</button>
+      <button class="mathBtn" @click="division">/</button>
+      <button class="mathBtn" @click="exponentiation">^</button>
     </div>
-    <input class="inputField" type="text" placeholder="число 2" />
+    <input
+      class="inputField"
+      v-model.number.trim="operand2"
+      type="text"
+      placeholder="число 2"
+    />
     <p>Сумма:</p>
     <div>{{ sum }}</div>
   </div>
@@ -24,6 +34,27 @@ export default {
       operand2: 0,
       sum: 0,
     };
+  },
+  methods: {
+    summary() {
+      this.sum = this.operand1 + this.operand2;
+    },
+    subtraction() {
+      this.sum = this.operand1 - this.operand2;
+    },
+    multiply() {
+      this.sum = this.operand1 * this.operand2;
+    },
+    division() {
+      if (this.operand2 === 0) {
+        this.sum = "Ошибка! Нельзя делить на ноль!";
+      } else {
+        this.sum = parseInt(this.operand1 / this.operand2);
+      }
+    },
+    exponentiation() {
+      this.sum = this.operand1 ** this.operand2;
+    },
   },
 };
 </script>
