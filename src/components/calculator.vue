8a31c4b4c8ad0d2675c5d76c25ed81df597dfa5f
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-06-24 17:05:40 +0300

    +

---
+++ src/components/calculator.vue
@@ -0,0 +1,22 @@
+<template>
+  <input type="text">
+  <input type="text">
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  data(){
+  return {
+  operand1: 0,
+  operand2: 0
+}
+}
+}
+</script>
+
+
+
+<style lang="scss">
+
+</style>
