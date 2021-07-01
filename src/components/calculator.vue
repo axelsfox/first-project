97fc3b2292455ba0960630da804d82608fe3a34b
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-07-01 22:04:35 +0300

    +

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -1,12 +1,45 @@
 <template>
-  <div>
+  <div class="mainBox">
     <p>Простой калькулятор</p>
     <input
       class="inputField"
+      @click="field = true"
       v-model.number.trim="operand1"
       type="text"
       placeholder="число 1"
     />
+    <input
+      class="inputField"
+      @click="field = false"
+      v-model.number.trim="operand2"
+      type="text"
+      placeholder="число 2"
+    />
+
+    <!--домашняя работа-->
+    <div>
+      <input type="checkbox" id="checkbox" v-model="checked" />
+      <label for="checkbox">Показать экранную клавиатуру</label>
+    </div>
+
+    <div class="screenKeyboard" v-if="checked == true">
+      <button
+        class="onekey"
+        v-for="screenkey in screenkeyboard"
+        :key="screenkey"
+        @click="boardButton(screenkey)"
+      >
+        {{ screenkey }}
+      </button>
+      <button class="onekey" @click="boardDelet">CE</button>
+    </div>
+    <input name="radioop" type="radio" id="one" :checked="field" />
+    <label for="one">Значение 1</label>
+    <br />
+    <input name="radioop" type="radio" id="two" :checked="!field" />
+    <label for="two">Значение 2</label>
+
+    <!---->
     <div class="mathBtnBox">
       <button
         class="mathBtn"
@@ -19,8 +52,9 @@
         {{ operand }}
       </button>
     </div>
-    <div v-if="error">Ошибка! {{ error }}</div>
 
+    <div v-if="error">Ошибка! {{ error }}</div>
+    <p>Результат: {{ result }}</p>
     <div class="strange-message">
       <template v-if="result < 0">Получилось отрицательное число</template>
       <template v-else-if="result < 100">Результат в первой сотне</template>
@@ -28,14 +62,6 @@
         >Получилось слишком большое число</template
       >
     </div>
-
-    <input
-      class="inputField"
-      v-model.number.trim="operand2"
-      type="text"
-      placeholder="число 2"
-    />
-    <p>Результат: {{ result }}</p>
     <div class="logs">
       <div v-for="(log, id) in logs" v-bind:key="id">{{ log }}</div>
     </div>
@@ -47,17 +73,23 @@ export default {
   name: "Calculator",
   data() {
     return {
-      operand1: 0,
-      operand2: 0,
+      operand1: "",
+      operand2: "",
+      field: true,
       result: 0,
       error: "",
       operands: ["+", "-", "*", "/", "^"],
       logs: {},
+      checked: false,
+      screenkeyboard: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     };
   },
   methods: {
+    boardButton(button) {
+      this.field ? (this.operand1 += button) : (this.operand2 += button);
+    },
     summary() {
-      this.result = this.operand1 + this.operand2;
+      this.result = Number(this.operand1) + Number(this.operand2);
     },
     subtraction() {
       this.result = this.operand1 - this.operand2;
@@ -99,6 +131,12 @@ export default {
         Date.now()
       ] = `${this.operand1}${operation}${this.operand2}=${this.result}`;
     },
+    boardDelet() {
+      this.field
+        ? (this.operand1 = this.operand1.slice(0, -1))
+        : (this.operand2 = this.operand2.slice(0, -1));
+    },
+    somesimple() {},
   },
 };
 </script>
@@ -113,7 +151,6 @@ export default {
   margin: 0% 30%;
 }
 .mathBtn {
-  height: 40px;
   width: 40px;
   margin: 10px;
   border: 3px solid black;
@@ -131,5 +168,22 @@ export default {
   border-radius: 15%;
   padding: 10px 10px;
   text-align: center;
+}
+.screenKeyboard {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 500px;
+  border: black 2px solid;
+  padding: 5px;
+  margin: 0px 700px;
+  box-sizing: border-box;
+}
+.onekey {
+  height: 30px;
+  width: 30px;
+  border: black 1px solid;
+  border-radius: 20%;
+  margin: 5px;
 }
 </style>
