492e4f2a528def92a5f8261d25d691fe6a45d615
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-07-05 10:36:53 +0300

    +

---
+++ les3_proj/money_manager/src/components/PaymentsDisplay.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="[$style.wrapper]">
+    {{ items }}
+  </div>
+</template>
+ 
+<script>
+export default {
+  props: ["items"],
+};
+</script>
+ 
+<style lang="scss" module>
+</style>
