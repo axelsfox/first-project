5eefd4d21ec7200ec6af83a618f61271283b6af5
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-07-07 17:04:08 +0300

    +

--- les3_proj/money_manager/src/App.vue
+++ les3_proj/money_manager/src/App.vue
@@ -4,6 +4,7 @@
       <h1>My personal costs</h1>
     </div>
     <div class="wrapper">
+      <AddPaymentForm @addNewPayment="addNewPaymentDate" />
       <PaymentsDisplay :items="paymentsList" />
     </div>
   </div>
@@ -11,11 +12,13 @@
  
 <script>
 import PaymentsDisplay from "./components/PaymentsDisplay";
+import AddPaymentForm from "./components/AddPaymentForm.vue";
 
 export default {
   name: "App",
   components: {
     PaymentsDisplay,
+    AddPaymentForm,
   },
   data() {
     return {
@@ -23,36 +26,44 @@ export default {
     };
   },
   methods: {
-    fetchData() {
-      return [
-        {
-          date: "12.03.2021",
-          category: "Food",
-          value: 180,
-        },
-        {
-          date: "12.03.2021",
-          category: "Internet",
-          value: 100,
-        },
-        {
-          date: "14.03.2021",
-          category: "Food",
-          value: 300,
-        },
-        {
-          date: "28.05.2021",
-          category: "Sport",
-          value: 180,
-        },
-      ];
+    addNewPaymentDate(value) {
+      console.log(value);
+      this.paymentsList = [...this.paymentsList, value];
     },
-  },
-  created() {
-    this.paymentsList = this.fetchData();
+    //fetchData() {
+    //  return [
+    //    {
+    //      date: "12.03.2021",
+    //      category: "Food",
+    //      value: 180,
+    //    },
+    //    {
+    //      date: "12.03.2021",
+    //      category: "Internet",
+    //      value: 100,
+    //    },
+    //    {
+    //      date: "14.03.2021",
+    //      category: "Food",
+    //      value: 300,
+    //    },
+    //    {
+    //      date: "28.05.2021",
+    //      category: "Sport",
+    //      value: 180,
+    //    },
+    //  ];
+    //},
+    //},
+    //created() {
+    //  this.paymentsList = this.fetchData();
   },
 };
 </script>
- 
-<style lang="scss" module>
+ <style>
+.header {
+  margin: 0px 30px;
+  color: rgb(53, 53, 53);
+  font-size: 18px;
+}
 </style>
