67fde39c3e255380d32a991a24ff151dd902a29c
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-07-30 15:33:36 +0300

    +

--- les3_proj/money_manager/src/main.js
+++ les3_proj/money_manager/src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import vuetify from '@/plugins/vuetify' 
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   store,
-  router
+  router,
+  vuetify
 }).$mount('#app')
 
